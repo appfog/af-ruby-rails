fa902a1d20626065619c2cc9cbe3945f98051f48
Lucas Carlson <lucas@rufy.com>
2012-04-16 14:26:00 -0700

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AfRubyRails::Application.config.secret_token = '1d89d22c372757b55ff2992a33a5091ac1e4599f4a6539316cd06608ad5bcbda14867939da238c6fa3918ab98c1b9b01f3bf8f8b75b719c970a6d7fbeb691015'
